97abd105e48379c22849491416d3df5a520f3d78
Egor Pobylets <egorpobylets@gmail.com>
2023-12-01 07:46:50 +0100

    feat: added config for global variables

---
+++ src/utils/generateResponseError.ts
@@ -0,0 +1,19 @@
+import { BadRequestException } from "@nestjs/common";
+import { ValidationError } from "class-validator";
+
+export const generateResponseError = (errors: ValidationError[]) => {
+  const errorsForResponse: {message: string, field: string}[] = []
+
+  errors.forEach(err => {
+    const constraintsKeys = Object.keys(err.constraints ?? {})
+    constraintsKeys.forEach(constraintsKey => {
+      errorsForResponse.push({
+        message: err.constraints ? err.constraints[constraintsKey] : '',
+        field: err.property,
+      })
+    })
+
+  })
+
+  throw new BadRequestException(errorsForResponse)
+}
