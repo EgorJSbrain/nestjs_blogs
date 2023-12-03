07290cb28ded4ad97c15fcc16e405ac852d9765b
Egor Pobylets <egorpobylets@gmail.com>
2023-12-03 09:15:43 +0100

    fix: fix basic guard

--- src/auth/strategies/basic.strategy.ts
+++ src/auth/strategies/basic.strategy.ts
@@ -2,19 +2,21 @@
 import { BasicStrategy as Strategy } from 'passport-http';
 import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ConfigService } from '@nestjs/config';
 import { UsersRepository } from 'src/users/users.repository';
 
 @Injectable()
 export class BasicAuthStrategy extends PassportStrategy(Strategy) {
     constructor(
-        private readonly configService: ConfigService,
         private readonly usersRepository: UsersRepository,
     ) {
         super();
     }
 
     public validate = async (username: string, password: string): Promise<boolean> => {
+      if (!password || !username) {
+        throw new UnauthorizedException();
+      }
+
       const user = await this.usersRepository.getUserByLoginOrEmail(username, username)
 
         if (!user) {
