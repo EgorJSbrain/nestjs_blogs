96e1016a86549e4fee78e7111cb54bba6880d7b7
Egor Pobylets <egorpobylets@gmail.com>
2023-12-04 08:08:48 +0100

    fix: added return

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -12,8 +12,6 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(loginOrEmail: string, password: string): Promise<{ userId: string }> {
-    console.log("validate----password:", password)
-    console.log("validate----loginOrEmail:", loginOrEmail)
     const user = await this.authRepository.verifyUser({ loginOrEmail, password })
 
     if (!user) {
