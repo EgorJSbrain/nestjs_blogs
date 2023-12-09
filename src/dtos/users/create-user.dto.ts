19fd9a71274b03b988cbd71eb9b76571c9acce76
Egor Pobylets <egorpobylets@gmail.com>
2023-12-09 08:17:15 +0100

    feat: added validation to paths; added validation

--- src/dtos/users/create-user.dto.ts
+++ src/dtos/users/create-user.dto.ts
@@ -1,17 +1,37 @@
-import { IsEmail, IsString, Length, Matches } from "class-validator"
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength
+} from 'class-validator'
+import {
+  EMAIL_REGEX,
+  LOGIN_MAX_LENGTH,
+  LOGIN_MIN_LENGTH,
+  LOGIN_REGEX,
+  PASSWORD_MAX_LENGTH,
+  PASSWORD_MIN_LENGTH,
+  PASSWORD_REGEX,
+  STRING_MAX_LENGTH
+} from '../../constants/global'
 
 export class CreateUserDto {
   @IsString()
-  @Length(3, 10)
-  @Matches(/^[a-zA-Z0-9_-]*$/)
+  @MinLength(LOGIN_MIN_LENGTH)
+  @MaxLength(LOGIN_MAX_LENGTH)
+  @Matches(LOGIN_REGEX)
   login: string
 
   @IsString()
-  @Length(6, 20)
+  @MinLength(PASSWORD_MIN_LENGTH)
+  @MaxLength(PASSWORD_MAX_LENGTH)
+  @Matches(PASSWORD_REGEX)
   password: string
 
   @IsString()
   @IsEmail()
-  @Matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
+  @MaxLength(STRING_MAX_LENGTH)
+  @Matches(EMAIL_REGEX)
   email: string
 }
