b91e35c738741710bbce1036e1fc3c926ea89133
Egor Pobylets <egorpobylets@gmail.com>
2023-12-09 08:37:50 +0100

    feat: fixed validation for blogs

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,11 +1,12 @@
-export const LOGIN_REGEX = /^[a-zA-Z0-9_-]*$/;
-export const LOGIN_MIN_LENGTH = 6;
-export const LOGIN_MAX_LENGTH = 6;
+export const LOGIN_MIN_LENGTH = 3;
+export const LOGIN_MAX_LENGTH = 10;
 export const PASSWORD_MIN_LENGTH = 6;
 export const PASSWORD_MAX_LENGTH = 20;
 export const STRING_MAX_LENGTH = 255;
+export const LOGIN_REGEX = /^[a-zA-Z0-9_-]*$/;
 export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z]).+$/;
-export const EMAIL_REGEX =
-  /^(?!.*[.!#$%&'*+/=?^_`{|}~-]{2,})(?![.!#$%&'*+/=?^_`{|}~-])(^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]{1,64})+(?<![.!#$%&'*+/=?^_`{|}~-])@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+export const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+// export const EMAIL_REGEX =
+//   /^(?!.*[.!#$%&'*+/=?^_`{|}~-]{2,})(?![.!#$%&'*+/=?^_`{|}~-])(^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]{1,64})+(?<![.!#$%&'*+/=?^_`{|}~-])@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 export const URL_REGEX =
   /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
