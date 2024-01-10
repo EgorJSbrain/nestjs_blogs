68d8506322aefd8921f29e268cc187baa97c08ba
Egor Pobylets <egorpobylets@gmail.com>
2024-01-10 08:15:26 +0100

    feat: added sql to blogs

--- src/blogs/blogs.repository.sql.ts
+++ src/blogs/blogs.repository.sql.ts
@@ -16,72 +16,48 @@ import { InjectDataSource } from '@nestjs/typeorm';
 export class BlogsSqlRepository {
   constructor(
     @InjectModel(Blog.name) private blogsModel: Model<BlogDocument>,
-    @InjectDataSource() protected dataSource: DataSource,
+    @InjectDataSource() protected dataSource: DataSource
   ) {}
 
-  async getAll(params: BlogsRequestParams): Promise<ResponseBody<BlogDocument> | []>  {
+  async getAll(
+    params: BlogsRequestParams
+  ): Promise<ResponseBody<BlogDocument> | []> {
     try {
       const {
         sortBy = 'createdAt',
         sortDirection = SortDirectionsEnum.desc,
         pageNumber = 1,
         pageSize = 10,
-        searchNameTerm = '',
+        searchNameTerm = ''
       } = params
 
-    const pageSizeNumber = Number(pageSize)
-    const pageNumberNum = Number(pageNumber)
-    const skip = (pageNumberNum - 1) * pageSizeNumber
+      const pageSizeNumber = Number(pageSize)
+      const pageNumberNum = Number(pageNumber)
+      const skip = (pageNumberNum - 1) * pageSizeNumber
 
-    const query = `
-      SELECT id, name, description, "createdAt", "websiteUrl", "isMembership"
-        FROM public.blogs
-        WHERE "name" ILIKE $1
-        ORDER BY "${sortBy}" ${sortDirection.toLocaleUpperCase()}
-        LIMIT $2 OFFSET $3
-    `
-    const blogs = await this.dataSource.query(query, [
-      `%${searchNameTerm}%`,
-      pageSizeNumber,
-      skip
-    ])
-    console.log("blogs:", blogs)
+      const query = `
+        SELECT id, name, description, "createdAt", "websiteUrl", "isMembership"
+          FROM public.blogs
+          WHERE "name" ILIKE $1
+          ORDER BY "${sortBy}" ${sortDirection.toLocaleUpperCase()}
+          LIMIT $2 OFFSET $3
+      `
+      const blogs = await this.dataSource.query(query, [
+        `%${searchNameTerm}%`,
+        pageSizeNumber,
+        skip
+      ])
 
-    const queryForCount = `
-      SELECT count(*) AS count FROM public.blogs
-        WHERE "name" ILIKE $1
-    `
-    const countResult = await this.dataSource.query(queryForCount, [
-      `%${searchNameTerm}%`
-    ])
-    console.log("---countResult:", countResult)
+      const queryForCount = `
+        SELECT count(*) AS count FROM public.blogs
+          WHERE "name" ILIKE $1
+      `
+      const countResult = await this.dataSource.query(queryForCount, [
+        `%${searchNameTerm}%`
+      ])
 
-    const count = countResult[0] ? Number(countResult[0].count) : 0
-    const pagesCount = Math.ceil(count / pageSizeNumber)
-
-      // const filter: FilterQuery<BlogDocument> = {}
-      // const sort: Record<string, SortOrder> = {}
-
-      // if (searchNameTerm) {
-      //   filter.name = { $regex: searchNameTerm, $options: 'i' }
-      // }
-
-      // if (sortBy && sortDirection) {
-      //   sort[sortBy] = sortDirection === SortDirectionsEnum.asc ? 1 : -1
-      // }
-
-      // const pageSizeNumber = Number(pageSize)
-      // const pageNumberNum = Number(pageNumber)
-      // const skip = (pageNumberNum - 1) * pageSizeNumber
-      // const count = await this.blogsModel.countDocuments(filter)
-      // const pagesCount = Math.ceil(count / pageSizeNumber)
-
-      // const blogs = await this.blogsModel
-      //   .find(filter, { _id: 0, __v: 0 })
-      //   .skip(skip)
-      //   .limit(pageSizeNumber)
-      //   .sort(sort)
-      //   .exec()
+      const count = countResult[0] ? Number(countResult[0].count) : 0
+      const pagesCount = Math.ceil(count / pageSizeNumber)
 
       return {
         pagesCount,
@@ -96,20 +72,18 @@ export class BlogsSqlRepository {
   }
 
   async getById(id: string): Promise<IBlog | null> {
-    const blog = await this.blogsModel.findOne({ id })
+    const query = `
+      SELECT id, name, description, "createdAt", "websiteUrl", "isMembership"
+      FROM public.blogs
+      WHERE id = $1
+    `
+    const blogs = await this.dataSource.query<IBlog[]>(query, [id])
 
-    if (!blog) {
+    if (!blogs[0]) {
       return null
     }
 
-    return {
-      id: blog.id,
-      name: blog.name,
-      description: blog.description,
-      websiteUrl: blog.websiteUrl,
-      isMembership: blog.isMembership,
-      createdAt: blog.createdAt,
-    }
+    return blogs[0]
   }
 
   async createBlog(data: CreateBlogDto): Promise<IBlog | null> {
@@ -121,33 +95,48 @@ export class BlogsSqlRepository {
         RETURNING id, name, description, "createdAt", "websiteUrl", "isMembership"
     `
 
-  const blogs = await this.dataSource.query(query, [
-    data.name,
-    data.description,
-    data.websiteUrl,
-  ])
+    const blogs = await this.dataSource.query(query, [
+      data.name,
+      data.description,
+      data.websiteUrl
+    ])
 
-  return blogs[0]
+    return blogs[0]
   }
 
-  async updateBlog(id: string, data: UpdateBlogDto): Promise<any> {
-    const blog = await this.blogsModel.findOne({ id })
+  async updateBlog(id: string, data: UpdateBlogDto): Promise<boolean> {
+    const blog = await this.getById(id)
 
     if (!blog) {
-      return null
+      return false
     }
 
-    blog.name = data.name ?? blog.name
-    blog.description = data.description ?? blog.description
-    blog.websiteUrl = data.websiteUrl ?? blog.websiteUrl
+    const name = data.name ?? blog.name
+    const description = data.description ?? blog.description
+    const websiteUrl = data.websiteUrl ?? blog.websiteUrl
 
-    blog.save()
+    const query = `
+      UPDATE public.blogs
+        SET "name"=$2, "description"=$3, "websiteUrl"=$4
+        WHERE id = $1;
+    `
+
+    await this.dataSource.query<IBlog[]>(query, [
+      id,
+      name,
+      description,
+      websiteUrl
+    ])
 
     return true
   }
 
   async deleteBlog(id: string) {
-    return await this.blogsModel.deleteOne({ id })
+    const query = `
+      DELETE FROM public.blogs
+      WHERE id = $1
+    `
+    return this.dataSource.query(query, [id])
   }
 
   save(blog: BlogDocument) {
