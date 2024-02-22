e4093516e3de3773cbb068c06b7ec724a8cbd455
Egor Pobylets <egorpobylets@gmail.com>
2024-02-22 07:58:27 +0100

    feat: added logic for questions for games

---
+++ src/entities/game-questions.ts
@@ -0,0 +1,67 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from 'typeorm'
+
+import { IGameQuestion } from '../types/game-question'
+import { GameEntity } from './game'
+import { QuestionEntity } from './question'
+
+@Entity({
+  name: 'game_questions'
+})
+
+export class GameQuestionEntity extends BaseEntity implements IGameQuestion {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column({ type: 'int', name: 'order' })
+  order: number
+
+  @Column()
+  gameId: string
+
+  @ManyToOne(() => GameEntity, game => game.questions)
+  @JoinColumn({
+    name: 'gameId'
+  })
+  game: GameEntity
+
+  @Column()
+  questionId: string
+
+  @ManyToOne(() => QuestionEntity)
+  @JoinColumn({
+    name: 'questionId'
+  })
+  question: QuestionEntity
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    name: 'createdAt',
+  })
+  createdAt: Date
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    nullable: true,
+    name: 'updatedAt',
+  })
+  updatedAt: Date | null;
+
+  @DeleteDateColumn({
+    type: 'timestamp',
+    nullable: true,
+    name: 'deletedAt',
+  })
+  deletedAt: Date | null;
+}
